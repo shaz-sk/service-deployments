b2ffd694e61cb245b5b5f1269cf54e7686767cec
shaz-sk <shanka900@gmail.com>
2025-01-02 10:45:19 +1100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,3 +8,5 @@ kubectl delete deployment/$SERVICE_NAME-deployment
 kubectl delete service/$SERVICE_NAME-service
 kubectl delete ingress/$SERVICE_NAME-ingress
 kubectl delete hpa/$SERVICE_NAME-hpa
+kubectl delete secret stock-ticker-secret
+kubectl delete configMap stock-ticker-config
